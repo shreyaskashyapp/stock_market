72cfe350e40b338689ee8a4c43d0157230fff642
Juice02 <tejasraghavendra.tr@gmail.com>
2023-06-08 18:46:29 +0530

    learner

--- frontend/src/Components/learn.js
+++ frontend/src/Components/learn.js
@@ -1,6 +1,8 @@
 import react from 'react'
 import '../css/learn.css';
 
+
+
 export default function Learn() {
   return (
     <div>
@@ -8,9 +10,10 @@ export default function Learn() {
         <p>SMA Stratergy</p>
       </div>
       <div className="videos">
+        <button onClick={()=>window.location = '/sma_intro'} className='intro'> Intro to SMA Stratergy</button>
+        <button onClick={()=>window.location = '/sma_intro'} className='learner'> Learn SMA Stratergy</button>
         <iframe width="310" height="200" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" allowFullScreen></iframe>
-        <iframe width="310" height="200" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" allowFullScreen></iframe>
-        <button className='quizbutt'> Take A Quiz</button>
+        <button onClick={()=>window.location = '/sma_quiz'} className='quizbutt'> Take A Quiz</button>
       </div>
 
       <div>
