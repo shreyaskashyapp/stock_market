ef7c6e819db0148c542426b61b3411f00a656eda
Juice02 <tejasraghavendra.tr@gmail.com>
2023-06-08 18:51:34 +0530

    a0pp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,11 @@ import Compete from "./Components/compete";
 import Profile from "./Components/profile";
 import Navbar from "./Components/navbar";
 import Smaquiz from "./Components/SMA_Quiz";
+import Smaintro from "./Components/LearningMaterial/intro_to_sma";
+import SmaLearn from "./Components/LearningMaterial/sma_learning";
+import SecIntro from "./Components/LearningMaterial/intro_to_strat2";
+import SecLearn from "./Components/LearningMaterial/strat2";
+
 
 import "./App.css";
 
@@ -20,6 +25,10 @@ function App() {
           <Route path="/compete" element={<Compete />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/sma_quiz" element={<Smaquiz />} />
+          <Route path="/sma_intro" element={<Smaintro />} />
+          <Route path="/sma_learn" element={<SmaLearn />} />
+          <Route path="/sec_intro" element={<SecIntro />} />
+          <Route path="/sec_learn" element={<SecLearn />} />
         </Routes>
       </Router>
     </div>
